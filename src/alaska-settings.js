769f2fd566411109b23090f1404ec8c11c682032
Liang Xingchen <liang@maichong.it>
2016-03-01 18:17:49 +0800

    init

---
+++ src/alaska-settings.js
@@ -0,0 +1,21 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-03-01
+ * @author Liang <liang@maichong.it>
+ */
+
+
+import alaska from 'alaska';
+import * as _ from 'lodash';
+
+/**
+ * @class SettingsService
+ */
+export default class SettingsService extends alaska.Service {
+  constructor(options, alaska) {
+    options = options || {};
+    options.id = 'alaska-settings';
+    options.dir = __dirname;
+    super(options, alaska);
+  }
+}
---
+++ src/config/alaska-settings.js
@@ -0,0 +1,9 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-03-01
+ * @author Liang <liang@maichong.it>
+ */
+
+export default{
+  prefix: '/settings'
+};
